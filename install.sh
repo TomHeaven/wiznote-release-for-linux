aa2f9515ed25a132928298ca3d69cfeb3e9351e1
TomHeaven <TomHeaven@qq.com>
2018-11-12 20:59:51 +0800

    update to 2.5.9

--- install.sh
+++ install.sh
@@ -1,9 +1,4 @@
 #/bin/sh
-echo "Note you need to install qt-opensource-linux-x64-5.7.0.run before you can install WizNote!"
-echo "Visit　https://download.qt.io/official_releases/qt/5.7/5.7.0/qt-opensource-linux-x64-5.7.0.run.mirrorlist to download and install qt5."
-echo "请先安装qt-opensource-linux-x64-5.7.0.run，再运行此脚本，否则为知笔记无法运行。"
-echo "访问https://download.qt.io/official_releases/qt/5.7/5.7.0/qt-opensource-linux-x64-5.7.0.run.mirrorlist以下载安装qt5。"
-
 
 # config
 INSTALL_DIR=/opt/wiznote
@@ -11,28 +6,17 @@ DESKTOP_DIR=~/桌面
 
 # copy files
 sudo mkdir -p $INSTALL_DIR/bin/
-
-sudo cp WizNote.bin $INSTALL_DIR/bin/
-sudo cp wiznote $INSTALL_DIR/bin/
-sudo cp wiznote.png $INSTALL_DIR/bin/
-sudo cp share.tar.gz $INSTALL_DIR/
-sudo tar -zxvf $INSTALL_DIR/share.tar.gz -C $INSTALL_DIR
-sudo rm -f $INSTALL_DIR/share.tar.gz
+sudo cp -f WizNote-2.5.9-x86_64.AppImage $INSTALL_DIR/bin/
+sudo cp -f wiznote $INSTALL_DIR/bin/
+sudo cp -f wiznote.png $INSTALL_DIR/bin/
 
 sudo cp -f wiznote.desktop /usr/share/applications/
 cp -f wiznote.desktop $DESKTOP_DIR/wiznote.desktop
 
-
 # fix privilleges
 sudo chmod a+x  $DESKTOP_DIR/wiznote.desktop
 sudo chmod a+x  $INSTALL_DIR/bin/wiznote
-
-# fix fcitx Chinese Input method
-sudo cp libfcitxplatforminputcontextplugin.so /opt/Qt5.7.0/Tools/QtCreator/lib/Qt/plugins/platforminputcontexts/libfcitxplatforminputcontextplugin.so
-sudo cp libfcitxplatforminputcontextplugin.so /opt/Qt5.7.0/5.7/gcc_64/plugins/platforminputcontexts/
-
-# fix qt5 library path
-echo "export LD_LIBRARY_PATH=/opt/Qt5.7.0/5.7/gcc_64/lib:\$LD_LIBRARY_PATH" | sudo tee -a /etc/profile
+sudo chmod a+x  $INSTALL_DIR/bin/WizNote-2.5.9-x86_64.AppImage
 
 # create links for terminal
 sudo ln -s -f  $INSTALL_DIR/bin/wiznote /usr/bin/wiznote
