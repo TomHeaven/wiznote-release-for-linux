309d520d748a2f6ed7f4b07520145863467c38eb
TomHeaven <TomHeaven@qq.com>
2019-12-27 17:47:45 +0800

    update to 2.7.9

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ DESKTOP_DIR=~/桌面
 
 # copy files
 sudo mkdir -p $INSTALL_DIR/bin/
-sudo cp -f WizNote-2.5.9-x86_64.AppImage $INSTALL_DIR/bin/
+sudo cp -f WizNote-2.7.9-x86_64.AppImage $INSTALL_DIR/bin/
 sudo cp -f wiznote $INSTALL_DIR/bin/
 sudo cp -f wiznote.png $INSTALL_DIR/bin/
 
@@ -16,7 +16,7 @@ cp -f wiznote.desktop $DESKTOP_DIR/wiznote.desktop
 # fix privilleges
 sudo chmod a+x  $DESKTOP_DIR/wiznote.desktop
 sudo chmod a+x  $INSTALL_DIR/bin/wiznote
-sudo chmod a+x  $INSTALL_DIR/bin/WizNote-2.5.9-x86_64.AppImage
+sudo chmod a+x  $INSTALL_DIR/bin/WizNote-2.7.9-x86_64.AppImage
 
 # create links for terminal
 sudo ln -s -f  $INSTALL_DIR/bin/wiznote /usr/bin/wiznote
